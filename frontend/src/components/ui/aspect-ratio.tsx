15a0b56dffdc58ba225c4dabd85193dce389b873
Zarrar <zarrarpeshimam@gmail.com>
2025-06-17 22:32:05 +0530

    Add generate button with API functionality

--- frontend/src/components/ui/aspect-ratio.tsx
+++ frontend/src/components/ui/aspect-ratio.tsx
@@ -1,5 +1,20 @@
-import * as AspectRatioPrimitive from "@radix-ui/react-aspect-ratio"
+import * as React from "react";
+import * as AspectRatioPrimitive from "@radix-ui/react-aspect-ratio";
+import { cn } from "@/lib/utils"; // utility for merging classNames
 
-const AspectRatio = AspectRatioPrimitive.Root
+const AspectRatio = React.forwardRef<
+  React.ElementRef<typeof AspectRatioPrimitive.Root>,
+  React.ComponentPropsWithoutRef<typeof AspectRatioPrimitive.Root> & {
+    className?: string;
+  }
+>(({ className, ...props }, ref) => (
+  <AspectRatioPrimitive.Root
+    ref={ref}
+    className={cn(className)}
+    {...props}
+  />
+));
 
-export { AspectRatio }
+AspectRatio.displayName = AspectRatioPrimitive.Root.displayName;
+
+export { AspectRatio };
